be7c543c400a735a9cbb3f0d03eba7384ba366ce
amberhandal <60497418+amberhandal@users.noreply.github.com>
2020-01-31 09:16:31 -0500

    Update BlackJack.java

--- BlackJack.java
+++ BlackJack.java
@@ -1,49 +1,52 @@
-import java.util.Random;
 import java.util.Scanner;
 
 public class BlackJack {
-    public static void main(String args[]) {
+    public static <round> void main(String args[]) {
         Scanner reading = new Scanner(System.in);
-        double menu;
-        boolean start = true;
-        int round = 1;
+
+        boolean startGame = true;
+        int[] round = {1};
         int card;
-        final int ACE; ACE = 1;
-        final int JACK; JACK = 11;
-        final int QUEEN; QUEEN = 12;
-        final int KING; KING = 13;
+        final int ACE = 1;
+        final int JACK = 11;
+        final int QUEEN = 12;
+        final int KING = 13;
+        int playerHand = 0;
+        int dealerHand = 0;
+        //Declaring variables for cards, rounds, and menus
 
+        while (startGame)
+        System.out.println("START GAME #" + round[1] + "\n");
+        //When game ends the round increases by 1 upon restarting
+        class P1Random {
+            P1Random rng = new P1Random();
+            int playerHand = rng.nextInt(10);
+            private void main() {
+                int menu = reading.nextInt();
+                System.out.println();
+                System.out.println("1. Get another card");
+                System.out.println("2. Hold hand");
+                System.out.println("3. Print statistics");
+                System.out.println("4. Exit");
+                System.out.println();
 
-        while (true) {
-            System.out.println("START GAME #" + round + "\n");
+                if (menu == 1) {
+                    if (playerHand == 21)
+                        System.out.println("You win!");
+                    else if (playerHand >= 21)
+                        System.out.println("You exceeded 21! You lose");
 
-                class P1Random {
-                    P1Random rng = new P1Random();
+                } else if (menu == 2) {
 
-                    private void main() {
-                    double menu;
-                        menu = reading.nextDouble();
-                        System.out.println();
-                        System.out.println("1. Get another card");
-                        System.out.println("2. Hold hand");
-                        System.out.println("3. Print statistics");
-                        System.out.println("4. Exit");
-                        System.out.println();
-                        if (menu == 1) {
+                } else if (menu == 3) {
 
-                        } else if (menu == 2) {
-
-                        } else if (menu == 3) {
-
-                        } else if (menu == 4) {
-                            return;
-                        } else {
-                            System.out.println("Invalid input!\n Please enter an integer value between 1 and 4.");
-                        }
-                    }
+                } else if (menu == 4) {
+                    return;
+                } else {
+                    System.out.println("Invalid input!\n Please enter an integer value between 1 and 4.");
                 }
-
-            round++;}
+            }
         }
+        round[0]++;
     }
-
+}
